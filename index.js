8d03af448cef60fd082d1c2550b295c7b664c19a
andrewregal <andrew.regal@gmail.com>
2017-03-13 11:55:23 -0700

    Complete basic functionality of tic-tac-toe

--- index.js
+++ index.js
@@ -13,20 +13,46 @@ var renderBoard = () => {
   console.log(row3.join(''));
 };
 
+var checkBoard = () => {
+  // search Xs
+  var XcheckColumnA = (row1[5] === 'X' && row2[5] === 'X' && row3[5] === 'X');
+  var XcheckColumnB = (row1[9] === 'X' && row2[9] === 'X' && row3[9] === 'X');
+  var XcheckColumnC = (row1[13] === 'X' && row2[13] === 'X' && row3[13] === 'X');
+  var XcheckRow1 = (row1[5] === 'X' && row1[9] === 'X' && row1[13] === 'X');
+  var XcheckRow2 = (row2[5] === 'X' && row2[9] === 'X' && row2[13] === 'X');
+  var XcheckRow3 = (row3[5] === 'X' && row3[9] === 'X' && row3[13] === 'X');
+  var XcheckDiagonal1 = (row1[5] === 'X' && row2[9] === 'X' && row3[13] === 'X');
+  var XcheckDiagonal2 = (row1[13] === 'X' && row2[9] === 'X' && row3[5] === 'X');
+
+  var OcheckColumnA = (row1[5] === 'O' && row2[5] === 'O' && row3[5] === 'O');
+  var OcheckColumnB = (row1[9] === 'O' && row2[9] === 'O' && row3[9] === 'O');
+  var OcheckColumnC = (row1[13] === 'O' && row2[13] === 'O' && row3[13] === 'O');
+  var OcheckRow1 = (row1[5] === 'O' && row1[9] === 'O' && row1[13] === 'O');
+  var OcheckRow2 = (row2[5] === 'O' && row2[9] === 'O' && row2[13] === 'O');
+  var OcheckRow3 = (row3[5] === 'O' && row3[9] === 'O' && row3[13] === 'O');
+  var OcheckDiagonal1 = (row1[5] === 'O' && row2[9] === 'O' && row3[13] === 'O');
+  var OcheckDiagonal2 = (row1[13] === 'O' && row2[9] === 'O' && row3[5] === 'O');
+
+  if (XcheckColumnA || XcheckColumnB || XcheckColumnC || XcheckRow1 || XcheckRow2 || XcheckRow3 || XcheckDiagonal1 || XcheckDiagonal2 || OcheckColumnA || OcheckColumnB || OcheckColumnC || OcheckRow1 || OcheckRow2 || OcheckRow3 || OcheckDiagonal1 || OcheckDiagonal2) {
+    foundWinner = true;
+  }
+};
+
 var changeBoard = (change) => {
-  currentTurnPlayer = (currentTurnPlayer === '1') ? '2' : '1';
+  XorO = (currentTurnPlayer === '1') ? 'X' : 'O';
   var validColumns = ['A', 'B', 'C'];
   var validRows = ['1', '2', '3'];
   if (validColumns.includes(change[0]) && validRows.includes(change[1]) && (change.length === 2)) {
     var letter = (change[0] === 'A') ? 5 : (change[0] === 'B') ? 9 : 13;
-    var row = (change[1] === '1') ? row1 : (change[1] === '2') ? row2 : row3;
+
     if (change[1] === '1') {
-      row1[letter] = 'X';
+      row1[letter] = XorO;
     } else if (change[1] === '2') {
-      row2[letter] = 'X';
+      row2[letter] = XorO;
     } else {
-      row3[letter] = 'X';
+      row3[letter] = XorO;
     }
+    checkBoard();
   } else {
     console.log('Invalid column or row');
   }
@@ -38,11 +64,15 @@ rl.setPrompt('Choose a Space  > ');
 rl.prompt();
 rl.on('line', (input) => {
   changeBoard(input);
-  renderBoard();
-  if (input === 'G6') {
+  if ((foundWinner === true) || (input === 'exit')) {
+    renderBoard();
+    console.log('**** Player ' + currentTurnPlayer + ' wins! ****');
     rl.close();
+  } else {
+    renderBoard();
+    currentTurnPlayer = (currentTurnPlayer === '1') ? '2' : '1';
+    rl.prompt();
   }
-  rl.prompt();
 }).on('close', () => {
   process.exit(0);
 });
