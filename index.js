382858549a4c33a1a423068c255925bb36980256
andrewregal <andrew.regal@gmail.com>
2017-03-13 10:53:11 -0700

    Add ability for users to select a board space

--- index.js
+++ index.js
@@ -1,26 +1,47 @@
 var readline = require('readline');
 
+String.prototype.replaceAt = function(index, character) {
+  return this.substr(0, index) + character + this.substr(index + character.length);
+};
+
 var row1 = ' 1 |   |   |   |';
 var row2 = ' 2 |   |   |   |';
 var row3 = ' 3 |   |   |   |';
 var foundWinner = false;
 
-console.log('   | A | B | C |');
-console.log(row1);
-console.log(row2);
-console.log(row3);
 
+var renderBoard = () => {
+  console.log('   | A | B | C |');
+  console.log(row1);
+  console.log(row2);
+  console.log(row3);
+};
 
 var changeBoard = (change) => {
-  if (change === 'A2') {
-    console.log('got here');
+  var validColumns = ['A', 'B', 'C'];
+  var validRows = ['1', '2', '3'];
+  if (validColumns.includes(change[0]) && validRows.includes(change[1]) && (change.length === 2)) {
+    var letter = (change[0] === 'A') ? 5 : (change[0] === 'B') ? 9 : 13;
+    var row = (change[1] === '1') ? row1 : (change[1] === '2') ? row2 : row3;
+    if (change[1] === '1') {
+      row1 = row1.replaceAt(letter, 'X');
+    } else if (change[1] === '2') {
+      row2 = row2.replaceAt(letter, 'X');
+    } else {
+      row3 = row3.replaceAt(letter, 'X');
+    }
+  } else {
+    console.log('Invalid column or row');
   }
 };
 
+renderBoard();
 var rl = readline.createInterface(process.stdin, process.stdout);
 rl.setPrompt('Choose a Space > ');
 rl.prompt();
 rl.on('line', (input) => {
+  changeBoard(input);
+  renderBoard();
   if (input === 'G6') {
     rl.close();
   }
